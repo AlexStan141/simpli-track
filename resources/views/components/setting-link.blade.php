17c462bbf119f23570c3ee8b5f3378ffc81ad754
StanNAlexandru <alexnstan@gmail.com>
2025-12-05 14:00:38 +0200

    Ascundere tab Priority

--- resources/views/components/setting-link.blade.php
+++ resources/views/components/setting-link.blade.php
@@ -1,10 +1,10 @@
-<div>
+<div class="grow">
     @if ($active)
-        <li class="flex justify-center w-[200px] pt-[11px] pb-[15px] font-nunito text-selectedsetting font-bold">
+        <li class="flex justify-center pt-[11px] pb-[15px] font-nunito text-selectedsetting font-bold">
             <a href="{{ $link }}" class="inline-block">{{$slot}}</a>
         </li>
     @else
-        <li class="flex justify-center w-[200px] border border-setting bg-bgsetting pt-[11px] pb-[15px] font-nunito">
+        <li class="flex justify-center border border-setting bg-bgsetting pt-[11px] pb-[15px] font-nunito">
             <a href="{{ $link }}" class="inline-block">{{$slot}}</a>
         </li>
     @endif
